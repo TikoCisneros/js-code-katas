2776d2901acb38824c91979d2902f501ec075fc6
Luis Cisneros <ext_lucisneros@falabella.cl>
2022-05-18 00:10:56 -0500

    add string sum code kata

---
+++ src/stringSum.js
@@ -0,0 +1,9 @@
+/**
+Write a simple String Sum utility with a function string Sum(string num1, string num2),
+which can accept only natural numbers and will return their sum.
+Replace entered number with 0 (zero) if entered number is not a natural number.
+ */
+
+const sum = (input) => input;
+
+export { sum };
