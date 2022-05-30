b037986ccf4648f8c83936379f75c00c52fa502b
Luis Cisneros <ext_lucisneros@falabella.cl>
2022-05-30 16:36:39 -0500

    add problem logic to make tests pass

--- src/stringSum.js
+++ src/stringSum.js
@@ -4,6 +4,18 @@ which can accept only natural numbers and will return their sum.
 Replace entered number with 0 (zero) if entered number is not a natural number.
  */
 
-const sum = (num1, num2) => 0;
+const sum = (num1, num2) => {
+  const summandOne = getNumericValueOrZero(num1);
+  const summandTwo = getNumericValueOrZero(num2);
+  return summandOne + summandTwo;
+};
+
+const getNumericValueOrZero = (num) => {
+  if (num.trim().length === 0) {
+    return 0;
+  }
+
+  return parseInt(num);
+}
 
 export { sum };
