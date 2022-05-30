032baecfb78f2e53d2ccbb7a73e9a564c8f07eec
Luis Cisneros <ext_lucisneros@falabella.cl>
2022-05-30 16:24:58 -0500

    add zero sum test

--- src/stringSum.spec.js
+++ src/stringSum.spec.js
@@ -7,4 +7,10 @@ describe('String Sum code kata', () => {
     const resp = sum(num1, num2);
     expect(resp).toBe(0);
   });
+  test('should return zero when num1 and num2 are 0 strings', () => {
+    const num1 = '0';
+    const num2 = '0';
+    const resp = sum(num1, num2);
+    expect(resp).toBe(0);
+  });
 });
