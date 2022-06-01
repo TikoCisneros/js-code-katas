4509cd74ae7f0eddf525fa49b06a841a7c7a0f36
Luis Cisneros <ext_lucisneros@falabella.cl>
2022-05-31 22:03:52 -0500

    pass the test where players select rock and scissors

--- src/rockPaperScissors.js
+++ src/rockPaperScissors.js
@@ -5,13 +5,20 @@ Rules:
 - Paper Beats Rock
 - Same Moves Result in Draw
 */
-
 export const GAME_MOVES = Object.freeze({
   rock: 'rock',
+  scissors: 'scissors',
 });
 
 export const GAME_RESULTS = Object.freeze({
   draw: 0,
+  playerOneWins: 1,
 });
 
-export const game = (playerOneMove, playerTwoMove) => GAME_RESULTS.draw;
+export const game = (playerOneMove, playerTwoMove) => {
+  if (playerOneMove === GAME_MOVES.rock && playerTwoMove === GAME_MOVES.scissors) {
+    return GAME_RESULTS.playerOneWins;
+  }
+
+  return GAME_RESULTS.draw;
+};
