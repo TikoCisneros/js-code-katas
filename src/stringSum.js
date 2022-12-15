956de25a25eedfec360c414fda364fde785d00a6
Luis Cisneros <ext_lucisneros@falabella.cl>
2022-12-15 10:07:42 -0500

    improve the description of the problems

--- src/stringSum.js
+++ src/stringSum.js
@@ -2,8 +2,9 @@
 Write a simple String Sum utility with a function string Sum(string num1, string num2),
 which can accept only natural numbers and will return their sum.
 Replace entered number with 0 (zero) if entered number is not a natural number.
- */
 
+Extra: accept more string numbers to get the addiction
+ */
 const sum = (input) => input;
 
 export { sum };
