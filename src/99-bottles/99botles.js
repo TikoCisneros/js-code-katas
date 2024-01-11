c870aa560c04236783c5c4115dbed80e394e1ee2
Luis Cisneros <tiko.cisneros@gmail.com>
2024-01-10 23:10:58 -0500

    check if number is one to pass the test

--- src/99-bottles/99botles.js
+++ src/99-bottles/99botles.js
@@ -1,5 +1,9 @@
 export class BeerSong {
   verse(number) {
+    if (number === 1) {
+      return "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n";
+    }
+
     return `${number} bottles of beer on the wall, ${number} bottles of beer.\nTake one down and pass it around, ${number - 1} bottles of beer on the wall.\n`;
   }
 }
