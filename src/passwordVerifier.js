e747f8b51a785f7417328d7e133f8d1f3b87d8f5
Luis Cisneros <tiko.cisneros@gmail.com>
2024-01-10 21:50:03 -0500

    add length validation to tests pass

--- src/passwordVerifier.js
+++ src/passwordVerifier.js
@@ -23,6 +23,10 @@ export class PasswordVerifier {
       throw Error("password should not be null")
     }
 
-    throw Error("password should be larger than 8 chars")
+    if(password.length <= 8) {
+      throw Error("password should be larger than 8 chars")
+    }
+    
+    throw Error("password should have one uppercase letter at least")
   }
 }
