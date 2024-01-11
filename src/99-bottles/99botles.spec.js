63278c9037ade3a2c0de46cfe4da1604a5512bb1
Luis Cisneros <tiko.cisneros@gmail.com>
2024-01-10 23:02:51 -0500

    add failed arbitrary verse test

--- src/99-bottles/99botles.spec.js
+++ src/99-bottles/99botles.spec.js
@@ -1,5 +1,11 @@
-describe('99 bottles', () => {
-  it('should test', () => {
-    expect(true).toBeTruthy();
+describe("99 bottles", () => {
+  it("should print an arbitrary verse", () => {
+    const expected =
+      "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n";
+    const beerSong = new BeerSong();
+    
+    const verse = beerSong.verse(8)
+
+    expect(verse).toBe(expected)
   });
 });
