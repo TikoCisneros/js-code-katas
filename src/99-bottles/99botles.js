09038d509199b2c92911c0925ef9103ad2c75107
Luis Cisneros <tiko.cisneros@gmail.com>
2024-01-10 23:34:18 -0500

    validate when number is 2 to fix the song text

--- src/99-bottles/99botles.js
+++ src/99-bottles/99botles.js
@@ -18,6 +18,6 @@ export class BeerSong {
       return "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n";
     }
 
-    return `${number} bottles of beer on the wall, ${number} bottles of beer.\nTake one down and pass it around, ${number - 1} bottles of beer on the wall.\n`;
+    return `${number} bottles of beer on the wall, ${number} bottles of beer.\nTake one down and pass it around, ${number - 1} ${number === 2 ? 'bottle' : 'bottles'} of beer on the wall.\n`;
   }
 }
--- src/99-bottles/99botles.spec.js
+++ src/99-bottles/99botles.spec.js
@@ -53,11 +53,11 @@ describe("99 bottles", () => {
   
   it("sings the rest of the verses", () => {
     const expected =
-    "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n\n2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n\n1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n";
+    "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n\n2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n\n1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.";
     const beerSong = new BeerSong();
 
     const actual = beerSong.sing(3);
 
-    expect(actual).toEqual(expected);
+    expect(actual).toBe(expected);
   });
 });
