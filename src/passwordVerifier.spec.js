0dac718efaccd67fa10e37e6ebb0f04910149087
Luis Cisneros <tiko.cisneros@gmail.com>
2024-01-10 21:37:36 -0500

    add longer password verification failed test

--- src/passwordVerifier.spec.js
+++ src/passwordVerifier.spec.js
@@ -6,4 +6,10 @@ describe('Password verifier', () => {
     const passVerifier = new PasswordVerifier();
     expect(() => passVerifier.verify(password)).toThrow(Error("password should not be null"));
   });
+
+  it('should throw error if password is not longer than 8 characters', () => {
+    const password = null;
+    const passVerifier = new PasswordVerifier();
+    expect(() => passVerifier.verify(password)).toThrow(Error("password should be larger than 8 chars"));
+  });
 });
