1620a1354ce868aa98fa2733cd90d31502dab493
Luis Alberto Cisneros Gómez <tiko.cisneros@gmail.com>
2024-05-21 16:16:43 -0500

    add fail test for filter characters bu gender

---
+++ src/http-kata/characters.spec.js
@@ -0,0 +1,25 @@
+import { getCharactersByNameOrGender } from "./characters";
+import * as api from "./api";
+jest.mock("./api.js");
+
+const API_RESULT_MOCK = [
+  {
+    id: 1,
+    name: "Rick Sanchez",
+    gender: "Male",
+  },
+  {
+    id: 2,
+    name: "Summer Smith",
+    gender: "Female",
+  },
+];
+
+describe("Characters functionality", () => {
+  it("should filter characters by gender successfully", async () => {
+    const output = [API_RESULT_MOCK[0]];
+    jest.spyOn(api, "getCharacters").mockReturnValue(API_RESULT_MOCK);
+    const result = await getCharactersByNameOrGender({ gender: "male" });
+    expect(result).toEqual(output);
+  });
+});
