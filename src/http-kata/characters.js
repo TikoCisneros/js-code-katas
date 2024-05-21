687cd7ad7c61414042ab576b40be8d4d8e320bff
Luis Alberto Cisneros Gómez <tiko.cisneros@gmail.com>
2024-05-21 16:25:23 -0500

    create filter by name functionality

--- src/http-kata/characters.js
+++ src/http-kata/characters.js
@@ -1,11 +1,15 @@
 import { getCharacters } from "./api";
 
-export async function getCharactersByNameOrGender({ gender }) {
+export async function getCharactersByNameOrGender({ gender, name }) {
   const characters = await getCharacters();
 
-  const filteredCharacters = characters.filter(
-    (character) => character.gender.toLowerCase() === gender.toLowerCase()
-  );
+  const filteredCharacters = characters.filter((character) => {
+    if (gender) {
+      return character.gender.toLowerCase() === gender.toLowerCase();
+    }
+
+    return character.name.toLowerCase().includes(name.toLowerCase());
+  });
 
   return filteredCharacters;
 }
